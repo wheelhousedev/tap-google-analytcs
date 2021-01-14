63e9bfb6c7323d16096d61fc1cafd3b7e33fe9ce
Aditya Sastry <adityas@hey.com>
2021-01-14 07:33:40 -0500

    add dim_ or met_ to column names

--- tap_google_analytics/ga_client.py
+++ tap_google_analytics/ga_client.py
@@ -233,11 +233,11 @@ class GAClient:
 
         for dimension in stream['dimensions']:
             report_definition['dimensions'].append(
-                {'name': dimension.replace("ga_dim_", "ga:")})
+                {'name': dimension.replace("dim_", "ga:")})
 
         for metric in stream['metrics']:
             report_definition['metrics'].append(
-                {"expression": metric.replace("ga_met_", "ga:")})
+                {"expression": metric.replace("met_", "ga:")})
 
         return report_definition
 
